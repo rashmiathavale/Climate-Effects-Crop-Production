6be552a589e7d59e8ca99920cc4d256e51c6c718
Huque, Zeba Fariha <zhuque3@gatech.edu>
2022-03-15 18:40:16 -0400

    title and state selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,43 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from constants import STATES
 
-"""
-baking
+st.title("Climate Effects on Crop Production in Midwestern United States")
+st.write('''  
+            Identify the configuration you would like to explore.       
+            ''')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+states = st.selectbox("Select a state. Some states are not yet available", STATES).strip()
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# """
+# # Welcome to Streamlit!
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
+# If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+# forums](https://discuss.streamlit.io).
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# In the meantime, below is an example of what you can do with just a few lines of code:
+# """
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# with st.echo(code_location='below'):
+#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    points_per_turn = total_points / num_turns
+#     Point = namedtuple('Point', 'x y')
+#     data = []
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+#     points_per_turn = total_points / num_turns
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+#     for curr_point_num in range(total_points):
+#         curr_turn, i = divmod(curr_point_num, points_per_turn)
+#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#         radius = curr_point_num / total_points
+#         x = radius * math.cos(angle)
+#         y = radius * math.sin(angle)
+#         data.append(Point(x, y))
+
+#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+#         .mark_circle(color='#0068c9', opacity=0.5)
+#         .encode(x='x:Q', y='y:Q'))
