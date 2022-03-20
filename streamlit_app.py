a743fd6512c1e3cc81763fb4eb24dc8240d81cfe
rashmiathavale <rashmi3434@gmail.com>
2022-03-19 17:33:30 -0700

    selectbox for counties

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,22 @@
 from collections import namedtuple
 import altair as alt
-import math
 import pandas as pd
 import streamlit as st
 from constants import STATES
+import matplotlib.pyplot as plt
+import csv
+
+filename = 'Crops.csv'
 
 st.title("Climate Effects on Crop Production")
 
 states = st.selectbox("Select a state", STATES).strip()
+COUNTIES = [];
+
+with open(filename, 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        if row[1] == states:
+            COUNTIES += row[0];
+
+counties = st.selectbox("Select a county", COUNTIES).strip()
