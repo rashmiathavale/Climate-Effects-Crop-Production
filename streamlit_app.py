ee455993a55726f3b27390ce7e85a00c069278ce
rashmiathavale <rashmi3434@gmail.com>
2022-03-19 17:47:32 -0700

    crops selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
-from collections import namedtuple
-import altair as alt
-import pandas as pd
 import streamlit as st
-from constants import STATES
+from constants import STATES, CROPS
 import csv
-
-filename = 'Crops.csv'
 
 st.title("Climate Effects on Crop Production")
 
-states = st.selectbox("Select a state", STATES).strip()
-COUNTIES = [];
+filename = 'Crops.csv'
+COUNTIES = []
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    states = st.selectbox("Select a state", STATES).strip()
 
 with open(filename, 'r') as csvfile:
     datareader = csv.reader(csvfile)
@@ -18,4 +18,8 @@ with open(filename, 'r') as csvfile:
         if row[1] == states:
             COUNTIES.append(row[0]);
 
-counties = st.selectbox("Select a county", COUNTIES).strip()
+with col2:
+    counties = st.selectbox("Select a county", COUNTIES).strip()
+
+with col3:
+    crops = st.selectbox("Select a state", CROPS).strip()
